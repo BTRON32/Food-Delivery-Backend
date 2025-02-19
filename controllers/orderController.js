149db14fd2eba0448aac3d5dab9a4a7f0a9d7c19
BTRON32 <131682089+BTRON32@users.noreply.github.com>
2025-02-19 21:02:54 +0530

    updated url in orderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -11,7 +11,7 @@ const placeOrder=async(req,res)=>{
 
     // const frontend_url="http://localhost:5173"
     // const frontend_url="http://localhost:5174"
-    const frontend_url="https://food-delivery-frontend-mew0.onrender.com/"
+    const frontend_url="https://food-delivery-frontend-mew0.onrender.com"
     try {
         const newOrder=new orderModel({
             userId:req.body.userId,
